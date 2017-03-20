49d4c2ca24d18535320f64039e7a0db4bd9f96b4
Horia Coman <horia141@gmail.com>
2017-03-20 10:42:00 +0200

    Added new middleware for authInfo extraction.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { execSync } from 'child_process'
 import * as express from 'express'
+import { MarshalFrom } from 'raynor'
+
+import { AuthInfo } from '@neoncity/identity-sdk-js'
+
+
+export interface Request extends express.Request {
+    authInfo: AuthInfo|null;
+}
 
 
 export function startupMigration() {
@@ -10,7 +18,7 @@ export function startupMigration() {
 export function newCorsMiddleware(clients: string[]): express.RequestHandler {
     const localClients = clients.slice(0);
     
-    return function(req: express.Request, res: express.Response, next: express.NextFunction): any {
+    return function(req: Request, res: express.Response, next: express.NextFunction): any {
 	// If origin is one of the allowed ones we emit an Access-Control-Allow-Origin header, with the
 	// specific origin (because only one is allowed). Otherwise, we omit it, and the resource won't
 	// be accessible in a CORS setting.
@@ -26,4 +34,32 @@ export function newCorsMiddleware(clients: string[]): express.RequestHandler {
 	// Fire away.
 	next();	
     };
+}
+
+
+export function newAuthInfoMiddleware(): express.RequestHandler {
+    const authInfoMarshaller = new (MarshalFrom(AuthInfo))();
+        
+    return function(req: Request, res: express.Response, next: express.NextFunction): any {
+	// Extract the auth info serialized header.
+	const authInfoSerialized: string|undefined = req.header('X-NeonCity-AuthInfo');
+	
+	if (typeof authInfoSerialized == 'undefined') {
+	    req.authInfo = null;
+	    next();
+	    return;
+	}
+
+	// Extract the auth info from the header.
+	try {
+	    req.authInfo = authInfoMarshaller.extract(JSON.parse(authInfoSerialized));
+	} catch (e) {
+	    res.status(400);
+	    res.end();
+	    return;
+	}
+	
+	// Fire away.
+	next();
+    }
 }
