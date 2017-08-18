b5576ff893bd5c9996ca0774ff75f070404b803f
Horia Coman <horia141@gmail.com>
2017-08-19 00:22:42 +0300

    Oh, just some modifications

--- src/logging-middleware.ts
+++ src/logging-middleware.ts
@@ -8,7 +8,7 @@ const BUNYAN_BUFFER_SIZE = 10;
 const BUNYAN_TIMEOUT_MS = 1000;
 
 
-export function newLoggingMiddleware(name: string, env: Env, bunyanToken: string|null = null, bunyanDomain: string|null = null) {
+export function newLoggingMiddleware(name: string, env: Env, logglyToken: string|null = null, logglySubdomain: string|null = null) {
     const streams = [];
 
     streams.push({
@@ -17,19 +17,19 @@ export function newLoggingMiddleware(name: string, env: Env, bunyanToken: string
     });
 
     if (env == Env.Staging || env == Env.Prod) {
-        if (bunyanToken == null || bunyanDomain == null) {
+        if (logglyToken == null || logglySubdomain == null) {
             throw new Error('In Staging and Prod Bunyan logging must be configured');
         }
 
-        const bunyanConfig = {
-            token: bunyanToken,
-            domain: bunyanDomain
+        const logglyConfig = {
+            token: logglyToken,
+            subdomain: logglySubdomain
         };
 
         streams.push({
             level: 'info',
             type: 'raw',
-            stream: new Bunyan2Loggly(bunyanConfig, BUNYAN_BUFFER_SIZE, BUNYAN_TIMEOUT_MS)
+            stream: new Bunyan2Loggly(logglyConfig, BUNYAN_BUFFER_SIZE, BUNYAN_TIMEOUT_MS)
         });
     }
 
